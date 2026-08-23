dce6c4d0252e010684190a718cb1164f6d84b88a
Matthew Mullori <smm200408@gmail.com>
2026-08-23 19:54:29 -0400

    swap

--- FileSystemElement.java
+++ FileSystemElement.java
@@ -1,4 +1,34 @@
 
-public interface FileSystemElement {
+public abstract class FileSystemElement {
+    String name;
+    FileSystemElement parent;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public FileSystemElement getParent() {
+        return this.parent;
+    }
+
+    public void setParent(FileSystemElement newParent) {
+        this.parent = newParent;
+    }
+
+    public void setName(String newName) {
+        if (newName == null) {
+            return;
+        }
+        if (newName.contains("/"))
+            this.name = newName;
+    }
+
+    public String getPath() {
+
+        if (this.getParent() == null) {
+            return this.getName();
+        }
+        return this.getName() + this.getParent().getPath();
+    }
 
 }
--- directory.java
+++ directory.java
@@ -1,3 +1,28 @@
-public class directory {
+import java.util.ArrayList;
 
+public class directory extends FileSystemElement {
+    ArrayList<FileSystemElement> contents;
+
+    public void add(String relativePath) {
+        if (relativePath == null) {
+            return;
+        }
+
+        if (relativePath.strip() == "") {
+            return;
+        }
+
+        int idx = relativePath.indexOf("/");
+
+        if(idx == -1) {
+            this.add(new File)
+        }
+
+    }
+
+    public void add(FileSystemElement element) {
+        this.contents.add(element);
+    }
+
+    public void removeElement(File)
 }
