4233b28f8555c5667501eed62327d82e8d569593
Matthew Mullori <smm200408@gmail.com>
2026-08-24 19:55:41 -0400

    the review is a bad person

--- File.java
+++ File.java
@@ -1,20 +1,8 @@
 public class File extends FileSystemElement {
-    String content;
 
     public File(String name, FileSystemElement parent) {
         this.name = "";
         this.setName(name);
         this.setParent(parent);
-    }
-
-    public void setContent(String newContent) {
-        if (newContent == null) {
-            return;
-        }
-        this.content = newContent;
-    }
-
-    public String getContent() {
-        return content;
     }
 }
